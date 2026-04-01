d428b4a9811f0a1e8e3d6db6c7c6e11ccc5d1105
Alex A <23cs007@drngpit.ac.in>
2026-04-01 18:56:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>st=new Stack<>();
+        Stack<Character>st = new Stack<>();
         for(int i=0;i<s.length();i++)
         {
-            char ch = s.charAt(i);
-            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{')
-            {
-                st.push(s.charAt(i));
-            }
+            Character ch = s.charAt(i);
+            if(ch.equals('(')||ch.equals('[')||ch.equals('{'))
+            st.push(ch);
             else
             {
-                if(st.isEmpty())
+                Character top=st.pop();
+                if(ch.equals(')')&&!top.equals('(')||ch.equals(']')&&!top.equals('[')||ch.equals('}')&&!top.equals('{'))
                 return false;
-                char top=st.pop();
-                if(top=='('&&ch!=')'||top=='['&&ch!=']'||top=='{'&&ch!='}')
-                return false;
+                
             }
-            
-        }return st.isEmpty();
+        }
+        return st.isEmpty();
     }
 }
