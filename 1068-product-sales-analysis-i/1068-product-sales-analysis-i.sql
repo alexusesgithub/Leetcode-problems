75f42de400b2b6c7264957ac5247c42113ca666c
Alex A <alex.data.dev44@gmail.com>
2026-06-13 11:55:21 +0530

    Time: 1798 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select p.product_name,w1.year,w1.price from Sales w1 left join product p on w1.product_id=p.product_id;
+select p.product_name,w1.year,w1.price
+ from Sales w1 left join product p 
+ on w1.product_id=p.product_id;
